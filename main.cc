f522cb63adffa366af6b751c8b8a2be2f84cb369
Faerryn <alexandre.liao@gmail.com>
2022-01-20 00:03:31 -0500

    automatically load core.forth

--- main.cc
+++ main.cc
@@ -1,15 +1,28 @@
 #include <cstdlib>
 #include <cstring>
+#include <filesystem>
 #include <fstream>
 #include <iostream>
-#include <optional>
 
 #include "engine.hh"
 #include "parser.hh"
 
-int main(int argc, char **argv) {
-  Engine engine;
+Engine engine;
 
+void evalFile(const std::filesystem::path &path) {
+  std::ifstream file{path};
+  if (!file.is_open()) {
+    std::cerr << path << ": No such file or directory\n";
+    exit(EXIT_FAILURE);
+  }
+  engine.evalIStream(file);
+  file.close();
+}
+
+int main(int argc, char **argv) {
+  std::filesystem::path selfPath{argv[0]};
+  std::filesystem::path corePath = selfPath.replace_filename("core.forth");
+  evalFile(corePath);
   if (argc == 1) {
     engine.evalIStream(std::cin);
   }
@@ -17,13 +30,7 @@ int main(int argc, char **argv) {
     if (std::strcmp(argv[i], "-") == 0) {
       engine.evalIStream(std::cin);
     } else {
-      std::ifstream file{argv[i]};
-      if (!file.is_open()) {
-        std::cerr << argv[i] << ": No such file or directory\n";
-        exit(EXIT_FAILURE);
-      }
-      engine.evalIStream(file);
-      file.close();
+      evalFile(argv[i]);
     }
   }
   return EXIT_SUCCESS;
